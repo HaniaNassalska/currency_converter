d58c15b1a699176c74be4729bcca42311ab42678
Hanna Nassalska <hanna.nassalska@gmail.com>
2022-04-04 21:38:34 +0200

    currency celection added

--- js/script.js
+++ js/script.js
@@ -1,30 +1,33 @@
-let amountElement = document.querySelector(".js-form__plnAmount");
-let resultElement = document.querySelector(".js-form__amountResult");
+const eurRate = 4.68;
+const usdRate = 4.22;
+const gbpRate = 5.55;
+let amountElement = document.querySelector(".js-plnAmount");
 let formElement = document.querySelector(".js-form");
-let currencyElement = document.querySelector("js-currencyOption");
+let currencyElement = document.querySelector(".js-currency");
+let resultElement = document.querySelector(".js-amountResult");
 
 
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
-    let eurRate = 4.68;
-    let usdRate = 4.22;
-    let gbpRate = 5.55;
 
     let plnAmount = +amountElement.value;
     let currency = currencyElement.value;
+
     let result;
+
     switch (currency) {
         case "USD":
             result = plnAmount / usdRate;
             break;
+
         case "EUR":
             result = plnAmount / eurRate;
             break;
+
         case "GBP":
             result = plnAmount / gbpRate;
-            break;        
+            break;
     }
-    console.log = result.toFixed(2);
-
+   resultElement.innerText = result.toFixed(2)
 });
