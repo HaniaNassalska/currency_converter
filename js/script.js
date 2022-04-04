9f25f9d8e844a028d75326e8a3ebe6088a0e95b4
Hanna Nassalska <hanna.nassalska@gmail.com>
2022-04-04 15:13:04 +0200

    js changed

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
-const eurRate = 4.68;
-let plnAmountElement = document.querySelector(".js-form__plnAmount");
-let eurResultElement = document.querySelector(".js-form__eurResult");
+let amountElement = document.querySelector(".js-form__plnAmount");
+let resultElement = document.querySelector(".js-form__amountResult");
 let formElement = document.querySelector(".js-form");
+let currencyElement = document.querySelector("js-currencyOption");
 
 
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
-    let plnAmount = plnAmountElement.value;
-    let eurResult = plnAmount / eurRate;
-    eurResultElement.innerText = eurResult.toFixed(2)
+    let eurRate = 4.68;
+    let usdRate = 4.22;
+    let gbpRate = 5.55;
+
+    let plnAmount = +amountElement.value;
+    let currency = currencyElement.value;
+    let result;
+    switch (currency) {
+        case "USD":
+            result = plnAmount / usdRate;
+            break;
+        case "EUR":
+            result = plnAmount / eurRate;
+            break;
+        case "GBP":
+            result = plnAmount / gbpRate;
+            break;        
+    }
+    console.log = result.toFixed(2);
 
 });
